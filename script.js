96281a5c443d2a918f56ec000b42788529dbf761
satork <sebastian.kedzier@gmail.com>
2024-04-19 16:17:12 +0200

    toggle input button function added plus stylization for it

--- script.js
+++ script.js
@@ -1,3 +1,120 @@
-1. wyszukanie loklalizacji (lokalization)
-2. przełączanie danych celsjusz/farenheit (celsjusz, farenheit)
-3. zmiana wyglądu w oparciu o pogode ()
+function localizationButtonSubmitShow() {
+  const locBtnSubmit = document.querySelector("#localizationButtonSubmit");
+  locBtnSubmit.classList.remove("locBtnSubmitHide");
+  locBtnSubmit.classList.add("locBtnSubmitShow");
+}
+function localizationButtonSubmitHide() {
+  const locBtnSubmit = document.querySelector("#localizationButtonSubmit");
+  locBtnSubmit.classList.remove("locBtnSubmitShow");
+  locBtnSubmit.classList.add("locBtnSubmitHide");
+}
+
+function localizationIOShow() {
+  const locOutput = document.querySelector("#localizationIO");
+  locOutput.classList.remove("locIOHide");
+  locOutput.classList.add("locIOShow");
+}
+
+function localizationIOHide() {
+  const locOutput = document.querySelector("#localizationIO");
+  locOutput.classList.remove("locIOShow");
+  locOutput.classList.add("locIOHide");
+}
+
+function toggleLocalizationInputShow() {
+  const locBtnOpen = document.querySelector("#localizationButtonOpen");
+  const locBtnSubmit = document.querySelector("#localizationButtonSubmit");
+
+  locBtnOpen.addEventListener("click", () => {
+    let trigger = locBtnSubmit.classList.contains("locBtnSubmitShow");
+    if (trigger) {
+      localizationButtonSubmitHide();
+      localizationIOHide();
+    } else {
+      localizationButtonSubmitShow();
+      localizationIOShow();
+    }
+  });
+}
+toggleLocalizationInputShow();
+
+
+
+
+
+
+
+
+// function getLocalization() {
+//   const localizationButtonSubmit = document.querySelector("#localizationButtonSubmit");
+//   const localizationInput = document.querySelector("#localizationOutput")
+//   localizationButtonSubmit.addEventListener("click", () => {
+//     const localizationInputValue = localizationInput.value
+//     return localizationInputValue;
+//   });
+// }
+
+
+
+// async function getParameters(localization) {
+//   try {
+//     const localizationInput = await localization;
+//     const apiKey = "6177da52a8f4428b8f5103747241704";
+//     const response = await fetch(
+//       `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${localizationInput}`,
+//       { mode: "cors" }
+//     );
+//     const currentData = await response.json();
+//     console.log(currentData);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// document.addEventListener("click", () => {
+//     console.log(getLocalization());
+// });
+
+// function skyChange() {
+//   const skyOutput = document.querySelector("#skyOutput img");
+// }
+
+// function dayNightChange() {}
+// function localizationInputChange() {
+//     const localizationButtonOpen = document.querySelector("#localizationButtonOpen")
+//     const localizationButtonSubmit = document.querySelector("#localizationButtonSubmit")
+//     localizationButtonOpen.addEventListener("click", () => {
+//       localizationButtonSubmit.classList.add()
+//     })
+// }
+// function temperatureChange() {}
+// function temperatureUnitChange() {}
+
+// const apiKey = "6177da52a8f4428b8f5103747241704";
+// fetch(
+//   `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=london`, {mode: "cors"}
+// )
+// .then(function(response) {
+//   return response.json();
+// })
+// .then(function(response) {
+//   console.log(response.current);
+// })
+// .catch(function(error) {
+//   console.log(error);
+// })
+
+// const img = document.querySelector('img');
+
+// async function getCats() {
+//     try {
+//         const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_ke=0cvagHFFWB3Iwao4Xz4m3VWvThsNlNK8&s=cats', {mode: 'cors'});
+//         const catData = await response.json()
+//         img.src = catData.data.images.original.url;
+//     } catch (error) {
+//         console.log(error)
+//         img.src = "logo.png"
+//     }
+// }
+
+// getCats()
